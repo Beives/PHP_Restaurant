82912fb475e69e46073492779c58dc9b2c82f9dc
Bakos_pc <b.bakos98@gmail.com>
2020-03-04 19:18:14 +0100

    Created layout and two sites

---
+++ includes/arrays.php
@@ -0,0 +1,42 @@
+<?php
+
+    $navItems = array(
+                    array(
+                        'slug' => "index.php",
+                        'title' => "Home"
+                    ),
+                    array(
+                        'slug' => "team.php",
+                        'title' => "Team"
+                    ),
+                    array(
+                        'slug' => "menu.php",
+                        'title' => "Menu"
+                    ),
+                    array(
+                        'slug' => "contact.php",
+                        'title' => "Contact"
+                    ),
+    );
+
+    $teamMembers = array(
+                    array(
+                        'name' => "Frankie",
+                        'position' => "Owner",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "frankie"
+                    ),
+                    array(
+                        'name' => "Francis",
+                        'position' => "General manager",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "francis"
+                    ),
+                    array(
+                        'name' => "Carlos",
+                        'position' => "Chef",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "carlos"
+                    )
+    );
+?>
